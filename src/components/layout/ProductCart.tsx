76192626b5dfb143f59de10463d4790b583d3896
hassaan-jumani <hassaanjumani@gmail.com>
2023-08-21 14:27:06 +0500

    Fixed-Stuff

--- src/components/layout/ProductCart.tsx
+++ src/components/layout/ProductCart.tsx
@@ -17,7 +17,6 @@ function ProductCart(props: {'title':string, 'price':number, 'img':string, 'cate
     <div className='py-6 transform motion-safe:hover:-translate-y-1 motion-safe:hover:scale-110 transition ease-in-out duration-300'>
       <div>
         <Image src={urlFor(props.img).url()} alt='first crousel' width={400} height={296}/>
-        {/* <img src={urlFor(props.img).width(400).url()} /> */}
         <h3 className='font-bold text-lg mt-3'> {props.title}</h3>
         <p className='text-lg'>${props.price}</p>
         <p className='text-lg font-bold'>Category: <span className='text-base font-normal capitalize'>{props.category}</span></p>
